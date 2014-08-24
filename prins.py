49f926b802c8efdf9b43b3a650468f5d6802d026
trsqxyz <trsqxyz@gmail.com>
2014-08-24 21:17:42 +0900

    Remove args type cast, write AttributeError for built-in module.

--- prins.py
+++ prins.py
@@ -4,10 +4,9 @@ class prins(object):
     """Printouts Python module source for text files."""
     def __init__(self):
         pass
-    def dropto(self, *modules):
+    def dropto(self, *names):
         """Prins drops to modules."""
         ms, notfound = [], []
-        names = iter(map(str, modules))
         for name in names:
             try:
                 ms.append((name, imp.find_module(name)[0]))
@@ -15,7 +14,10 @@ class prins(object):
                 notfound.append(name)
         for n,m in ms:
             with open("{0}.txt".format(n), "w") as f:
-                f.write(m.read())
-                m.close()
+                try:
+                    f.write(m.read())
+                    m.close()
+                except AttributeError:
+                    notfound.append(n)
         if notfound:
             print("Not Found {0}.".format(", ".join(map(str, notfound))))
